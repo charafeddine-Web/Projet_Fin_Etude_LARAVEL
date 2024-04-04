1f4804b40beedb82c8093dab8e2bf50320a06ef4
charafeddine-Web <charafeddinetbibzat@gmail.com>
2024-04-04 14:38:43 +0000

    new modification

--- routes/web.php
+++ routes/web.php
@@ -6,7 +6,7 @@ use App\Http\Controllers\ClasseController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\AdminController;
 use App\Http\Controllers\UserController;
-use App\Http\Controllers\AboutController;
+//use App\Http\Controllers\AboutController;
 
 /*
 |--------------------------------------------------------------------------
